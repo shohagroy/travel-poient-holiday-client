128039e02f223255fa4e9415d91baf647ceb8175
shohagroy <pkshohag240@gmail.com>
2022-11-08 22:57:47 +0600

    review section get and post method added and update

--- src/Components/Shared/ServicesDetails/ServicesDetalis.js
+++ src/Components/Shared/ServicesDetails/ServicesDetalis.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthProvaider } from "../../GlobalContext/GobalContext";
 import swal from "sweetalert";
+import ReviewCard from "./ReviewCard";
 
 const ServicesDetalis = () => {
   const services = useLoaderData()[0];
@@ -19,8 +20,10 @@ const ServicesDetalis = () => {
     const clientReview = {
       review,
       userReating,
+      postTime: new Date().toLocaleString() + " ",
       user,
       services,
+      postID: services._id,
     };
 
     fetch("http://localhost:5000/review", {
@@ -47,13 +50,13 @@ const ServicesDetalis = () => {
   };
 
   useEffect(() => {
-    fetch("http://localhost:5000/reviews")
+    fetch(`http://localhost:5000/reviews?id=${services._id}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setReviews(data);
       });
-  }, []);
+  }, [reating]);
 
   console.log(reviews);
 
@@ -127,159 +130,9 @@ const ServicesDetalis = () => {
                   </h1>
                 </div>
                 <div className="container flex flex-col items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
-                  <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
-                    <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-100">
-                      <p className="relative px-6 py-1 text-lg italic text-center text-gray-900">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          fill="currentColor"
-                          className="w-8 h-8 text-red-600"
-                        >
-                          <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-                          <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                        </svg>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Voluptatibus quibusdam, eligendi exercitationem
-                        molestias possimus facere.
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          fill="currentColor"
-                          className="absolute right-0 w-8 h-8 text-red-600"
-                        >
-                          <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-                          <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-                        </svg>
-                      </p>
-                    </div>
-                    <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-red-600 text-white">
-                      <img
-                        src="https://source.unsplash.com/50x50/?portrait?1"
-                        alt=""
-                        className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 bg-gray-700"
-                      />
-                      <p className="text-xl font-semibold leading-tight">
-                        Distinctio Animi
-                      </p>
-                      <p className="text-sm uppercase">Aliquam illum</p>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
-                    <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-900">
-                      <p className="relative px-6 py-1 text-lg italic text-center text-gray-100">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          fill="currentColor"
-                          className="w-8 h-8 text-violet-400"
-                        >
-                          <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-                          <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                        </svg>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Voluptatibus quibusdam, eligendi exercitationem
-                        molestias possimus facere.
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          fill="currentColor"
-                          className="absolute right-0 w-8 h-8 text-violet-400"
-                        >
-                          <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-                          <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-                        </svg>
-                      </p>
-                    </div>
-                    <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-violet-400 text-gray-900">
-                      <img
-                        src="https://source.unsplash.com/50x50/?portrait?2"
-                        alt=""
-                        className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 bg-gray-700"
-                      />
-                      <p className="text-xl font-semibold leading-tight">
-                        Distinctio Animi
-                      </p>
-                      <p className="text-sm uppercase">Aliquam illum</p>
-                    </div>
-                  </div>
-                  <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
-                    <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-900">
-                      <p className="relative px-6 py-1 text-lg italic text-center text-gray-100">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          fill="currentColor"
-                          className="w-8 h-8 text-violet-400"
-                        >
-                          <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-                          <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                        </svg>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Voluptatibus quibusdam, eligendi exercitationem
-                        molestias possimus facere.
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          fill="currentColor"
-                          className="absolute right-0 w-8 h-8 text-violet-400"
-                        >
-                          <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-                          <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-                        </svg>
-                      </p>
-                    </div>
-                    <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-violet-400 text-gray-900">
-                      <img
-                        src="https://source.unsplash.com/50x50/?portrait?3"
-                        alt=""
-                        className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 bg-gray-700"
-                      />
-                      <p className="text-xl font-semibold leading-tight">
-                        Distinctio Animi
-                      </p>
-                      <p className="text-sm uppercase">Aliquam illum</p>
-                    </div>
-                  </div>
-                  <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
-                    <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-900">
-                      <p className="relative px-6 py-1 text-lg italic text-center text-gray-100">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          fill="currentColor"
-                          className="w-8 h-8 text-violet-400"
-                        >
-                          <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-                          <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                        </svg>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Voluptatibus quibusdam, eligendi exercitationem
-                        molestias possimus facere.
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          fill="currentColor"
-                          className="absolute right-0 w-8 h-8 text-violet-400"
-                        >
-                          <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-                          <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-                        </svg>
-                      </p>
-                    </div>
-                    <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-violet-400 text-gray-900">
-                      <img
-                        src="https://source.unsplash.com/50x50/?portrait?4"
-                        alt=""
-                        className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 bg-gray-700"
-                      />
-                      <p className="text-xl font-semibold leading-tight">
-                        Distinctio Animi
-                      </p>
-                      <p className="text-sm uppercase">Aliquam illum</p>
-                    </div>
-                  </div>
+                  {reviews.map((review) => (
+                    <ReviewCard userReview={review} key={review._id} />
+                  ))}
                 </div>
               </section>
             </div>
